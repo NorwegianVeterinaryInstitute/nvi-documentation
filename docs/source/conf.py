8c3d540def92ae6f7360a0cdbd917fbdd5e58d5f
novica <nnovica@gmail.com>
2023-10-09 14:32:46 +0200

    add theme as extension

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -31,6 +31,7 @@ release = 'v0.0.1'
 # extensions coming with Sphinx (named 'sphinx.ext.*') or your custom
 # ones.
 extensions = [
+    'sphinx_rtd_theme',
 ]
 
 # Add any paths that contain templates here, relative to this directory.
@@ -46,7 +47,7 @@ exclude_patterns = []
 
 # The theme to use for HTML and HTML Help pages.  See the documentation for
 # a list of builtin themes.
-#
+
 html_theme = 'sphinx_rtd_theme'
 
 # Add any paths that contain custom static files (such as style sheets) here,
